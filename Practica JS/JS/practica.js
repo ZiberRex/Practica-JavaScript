1449a6d2fe6b575e7ab7e9a629fdec9371f3c9ed
EmilioM <62017222+ZiberRex@users.noreply.github.com>
2023-03-29 15:51:01 -0400

    Creacion de Funcion para sumar

--- Practica JS/JS/practica.js
+++ Practica JS/JS/practica.js
@@ -6,3 +6,13 @@ function nombre(){
 }
 
 var funcion = nombre();
+
+//Funcion suma
+
+function suma(){
+    var a = 45;
+    var b = 6;
+    console.log("La suma  es: " + (a + b));
+}
+
+var sumafuncion = suma();
